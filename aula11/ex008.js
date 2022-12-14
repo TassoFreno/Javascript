04ea3d7959cfe906cc75b92003bbe2ad3fe50c65
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-14 18:13:44 -0300

    cmtg

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('O console Funcionou corretamente')
+var vel = 78.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // dentro da condicao está o que voce deseja ( nao esquece)
+    console.log('Voce Ultrapassou a velocidade permitida.Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
