d85d0aec71f3ca89240056bd0b284f7acd75df75
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-16 23:33:16 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,10 +1,5 @@
 var idade = 22
 
-
 if (idade < 16) {
-    console.log('Nao Vota!')
-} else {
-    if (idade >= 16 && idade < 18) {
-        console.log('Voto opcional')
-    }
-}
+    console.log('Nao vota') 
+} 
