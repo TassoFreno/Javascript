83bbacf61ec53851ceeff3c01f0f8ceafe10cffa
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-21 15:26:18 -0300

    c ccc

--- Aula 12 ex/modelo/scripj.js
+++ Aula 12 ex/modelo/scripj.js
@@ -1,1 +1,59 @@
-window.alert('ola')
+const { image } = require("html-to-text/lib/formatter")
+
+function verificar() {
+    
+    
+    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[Erro] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0] .checked) {
+            genero = 'Homem'
+            if(idade >=0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'bebe-h.jpg')
+               }   else if (idade < 21) {
+                    //jovem
+                    img.setAttribute('src', 'jovens-h.png')
+            } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'homem-adulto.png')
+              }  else {
+                //idoso
+                img.setAttribute('src', 'idoso.png')
+              }
+             } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if(idade >=0 && idade < 10) {
+                // crianca
+                img.setAttribute('src', 'bebe-m.png')
+               }   else if (idade < 21) {
+                    //jovem
+                    img.setAttribute('src', 'jovens-m.png')
+            } else if (idade < 50) {
+            //adulto
+            img.setAttribute('src', 'mulher-adulto.png')
+              }  else {
+                //idoso
+                img.setAttribute('src', 'idosa.png')
+              }
+
+             }
+
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos ${genero} com ${idade} Anos.`
+            res.appendChild(img)
+
+    }
+
+}
+     
