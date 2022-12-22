bd6279a1a34a8f9c69311583adefd7fdf31a65e0
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-22 12:05:58 -0300

    c,mt

--- aula 14/ambiente.js
+++ aula 14/ambiente.js
@@ -1,5 +1,19 @@
-var c = 1 
+/*var c = 1 
 while (c <= 5) {
     console.log(c)
     c++
 }
+*/
+
+/*for( var c = 1; c <= 70; c++)  {
+    console.log(c)
+
+}
+
+*/
+
+console.log('vai comecar...')
+for(var c = 1; c<= 4; c++) {
+console.log(c) 
+}
+console.log('FIM')
