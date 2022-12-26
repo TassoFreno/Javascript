e6df9493c25167ab36d11eff6742625697cdaf80
TassoFreno <118383460+TassoFreno@users.noreply.github.com>
2022-12-26 13:05:49 -0300

    XXXX

---
+++ aula 15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5, 8, 4, 6, 7]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posicoes`)
+console.log(num[0])
+let pos = num.indexOf(9)
+console.log(`O valor 8 esta na ${pos}`)
---
+++ aula 16/function03.js
@@ -0,0 +1,7 @@
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(6))
+
+//exemplo de como é possivel alocar funcoes em variaveis
